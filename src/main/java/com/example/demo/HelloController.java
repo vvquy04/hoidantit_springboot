cabe765397d3b67b9368ba6eb54bf4a9da54c583
Văn Quý <vivanquy20804@gmail.com>
2025-07-21 14:09:01 +0700

    video 36

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -7,6 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World with Hỏi dân it!";
+        return "Hello World update!";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can access!";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can access!";
     }
 }
--- src/main/java/com/example/demo/HoidanItSpringBootApplication.java
+++ src/main/java/com/example/demo/HoidanItSpringBootApplication.java
@@ -2,12 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class HoidanItSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HoidanItSpringBootApplication.class, args);
+		
 	}
 
 }
