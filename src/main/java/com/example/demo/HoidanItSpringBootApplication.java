ed07e334283f9480abeba4f206d5c3ee9334d0b7
Văn Quý <vivanquy20804@gmail.com>
2025-07-25 15:14:35 +0700

    video 53

--- src/main/java/com/example/demo/HoidanItSpringBootApplication.java
+++ src/main/java/com/example/demo/HoidanItSpringBootApplication.java
@@ -2,11 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude =
 org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
-public class HoidanItSpringBootApplication {
+public class HoidanItSpringBootApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(HoidanItSpringBootApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(HoidanItSpringBootApplication.class, args);
 		
--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,34 +2,44 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+
+import com.example.demo.domain.User;
 
 import com.example.demo.service.UserService;
 
-
-// @Controller
-// public class UserController {
-
-//     @RequestMapping("/")
-//     public String getHomePage() {
-//         return "hello from controller";
-//     }
-// }
-
-@RestController
+@Controller
 public class UserController {
-
     private UserService userService;
-    
+
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("/")
-    public String getHomePage() {
-        return this.userService.HandleHello();
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        String test = this.userService.HandleHello();
+        model.addAttribute("eric", test);
+        model.addAttribute("hoidanit", "chào cưng");
+        return "hello";
     }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
+        System.out.println("run here "+hoidanit);
+        return "hello";
+    }
+    
+
 }
